d25822d12427d5e56803f7fd9a94adda98376fd8
PrabhashDiss <101409417+PrabhashDiss@users.noreply.github.com>
2025-10-06 14:01:27 +0530

    feat: remove fugitive plugin configuration and add it directly in init.lua

--- init.lua
+++ init.lua
@@ -1064,7 +1064,9 @@ require('lazy').setup({
   -- require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'kickstart.plugins.harpoon', -- adds harpoon with keymaps
-  require 'kickstart.plugins.fugitive', -- git integration
+  
+  -- Git integration plugin
+  'tpope/vim-fugitive',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
