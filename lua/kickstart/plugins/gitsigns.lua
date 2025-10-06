5cda5974aeb7c553a13af7120aa83d2f3dcffe63
PrabhashDiss <101409417+PrabhashDiss@users.noreply.github.com>
2025-10-06 15:21:10 +0530

    feat: add current line blame configuration to gitsigns

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -6,6 +6,13 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     opts = {
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = false,
+      },
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
