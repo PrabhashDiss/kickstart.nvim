391e088015a2b4874e1e2ce39266ffa84051b417
PrabhashDiss <101409417+PrabhashDiss@users.noreply.github.com>
2025-10-15 10:23:30 +0530

    config: add configuration function for CodeCompanion plugin

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -10,4 +10,10 @@ return {
       git_commit = require 'custom.plugins.codecompanion.prompts.git_commit',
     },
   },
+  config = function(_, opts)
+    require('codecompanion').setup(opts)
+    vim.keymap.set('n', '<leader>cc', function()
+      require('codecompanion').toggle()
+    end, { desc = 'Toggle CodeCompanion' })
+  end,
 }
