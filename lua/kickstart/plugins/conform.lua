020a132c83e7362c9f9ef3810599cbd2d37a1fe2
PrabhashDiss <101409417+PrabhashDiss@users.noreply.github.com>
2025-12-13 10:15:24 +0530

    Add explanatory comments to conform.lua formatters

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -34,9 +34,11 @@ return {
     formatters_by_ft = {
       lua = { 'stylua' },
       -- Conform can also run multiple formatters sequentially
+      -- isort: sorts imports, black: formats code
       python = { 'isort', 'black' },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
+      -- prettierd: faster daemon version, prettier: fallback
       javascript = { 'prettierd', 'prettier', stop_after_first = true },
     },
   },
